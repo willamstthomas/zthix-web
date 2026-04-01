0f66f36826ad0476e7e77ffbe369ef3f61fe94c9
willamstthomas <info@zthix.com>
2026-04-01 06:25:33 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/verify/:path*",
+        destination: "https://zthix-broker-terminal.vercel.app/verify/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
